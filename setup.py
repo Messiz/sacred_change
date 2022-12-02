0bcb428049afa3d68eaa9b9deb83aa4fb8d8e8d0
Messiz <44800799+Messiz@users.noreply.github.com>
2022-12-02 12:58:40 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
         'docopt==0.3',
         'jsonpickle==0.7.2',
         'munch==2.0.2',
-        'wrapt==1.0'
+#         'wrapt==1.0'
     ],
     tests_require=[
         'mock>=0.8, <3.0',
